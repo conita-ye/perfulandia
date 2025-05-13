c3094e322ad0b34094a2d5c459f848f5c271539c
macarena <mac.espinoza@duocuc.cl>
2025-05-13 09:54:38 -0400

    msvc-carritocompras

---
+++ msvc-carritocompras/src/main/java/com/macaesp/msvc/carritocompras/MsvcCarritocomprasApplication.java
@@ -0,0 +1,13 @@
+package com.macaesp.msvc.carritocompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcCarritocomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcCarritocomprasApplication.class, args);
+	}
+
+}
