17e764b6f6835893514f3cc2f358ca02cafedcb1
effimrv <ara.escobar@duocuc.cl>
2025-05-13 09:43:56 -0400

    msvc-cliente

---
+++ msvc-cliente/src/test/java/com/effimrv/msvc/cliente/msvc_cliente/MsvcClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.effimrv.msvc.cliente.msvc_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
