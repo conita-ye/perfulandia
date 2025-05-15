fd1b16b09b989a73589025f6fe10173844666414
maca <mac.espinoza@duocuc.cl>
2025-05-15 09:27:51 -0400

    commit

--- msvc-sucursal/src/main/java/com/example/msvc/sucursal/model/Sucursal.java
+++ msvc-sucursal/src/main/java/com/example/msvc/sucursal/model/Sucursal.java
@@ -5,12 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Sucursal {
@@ -20,36 +19,4 @@ public class Sucursal {
     private String Nombre;
     private String Direccion;
     private String Ciudad;
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public String getNombre() {
-        return Nombre;
-    }
-
-    public void setNombre(String nombre) {
-        Nombre = nombre;
-    }
-
-    public String getDireccion() {
-        return Direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        Direccion = direccion;
-    }
-
-    public String getCiudad() {
-        return Ciudad;
-    }
-
-    public void setCiudad(String ciudad) {
-        Ciudad = ciudad;
-    }
 }
