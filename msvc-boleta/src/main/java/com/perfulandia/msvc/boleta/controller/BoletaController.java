6e6a38a6a206f583d259aba98db3bc10fe400701
effimrv <ara.escobar@duocuc.cl>
2025-05-24 18:14:49 -0400

    commmit

--- msvc-boleta/src/main/java/com/perfulandia/msvc/boleta/controller/BoletaController.java
+++ msvc-boleta/src/main/java/com/perfulandia/msvc/boleta/controller/BoletaController.java
@@ -1,4 +1,38 @@
 package com.perfulandia.msvc.boleta.controller;
 
+import com.perfulandia.msvc.boleta.model.entities.Boleta;
+import com.perfulandia.msvc.boleta.service.BoletaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/boletas")
+@Validated
+
 public class BoletaController {
+    @Autowired
+    private BoletaService boletaService;
+
+    @GetMapping
+    public ResponseEntity<List<Boleta>> findAll () {
+        return ResponseEntity.status(HttpStatus.OK).body(boletaService.finAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Boleta> findById (@PathVariable long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(boletaService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Boleta> save (@RequestBody @Valid Boleta boleta) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(boletaService.save(boleta));
+    }
+
+
 }
--- msvc-cliente/src/main/java/com/perfulandia/msvc/cliente/controller/ClienteController.java
+++
@@ -1,4 +0,0 @@
-package com.perfulandia.msvc.cliente.controller;
-
-public class ClienteController {
-}
