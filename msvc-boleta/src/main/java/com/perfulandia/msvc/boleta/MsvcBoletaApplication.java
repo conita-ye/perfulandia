5ee1241dadba58e50f5fd03e7d1ceb23e57244c8
macaesp <mac.espinoza@duocuc.cl>
2025-05-27 00:15:30 -0400

    commit

---
+++ msvc-boleta/src/main/java/com/perfulandia/msvc/boleta/MsvcBoletaApplication.java
@@ -0,0 +1,9 @@
+package com.perfulandia.msvc.boleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcBoletaApplication {
+    public static void main(String[] args) { SpringApplication.run(MsvcBoletaApplication.class, args); }
+}
