ace98bcd849b57e2dc6dfd8ccaf5b73b9feb4835
macaesp <mac.espinoza@duocuc.cl>
2025-05-27 10:26:37 -0400

    commit

--- msvc-sucursal/src/main/java/com/perulandia/msvc/sucursal/service/SucursalService.java
+++ msvc-sucursal/src/main/java/com/perulandia/msvc/sucursal/service/SucursalService.java
@@ -1,11 +1,10 @@
 package com.perulandia.msvc.sucursal.service;
 
 import com.perulandia.msvc.sucursal.model.Sucursal;
-
 import java.util.List;
 
 public interface SucursalService {
-    List<Sucusal> findAll ();
+    List<Sucursal> findAll ();
     Sucursal findById (Long id);
     Sucursal save (Sucursal sucursal);
 }
