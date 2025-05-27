93b1264f8cbce920cf736e0b2de5dc99bad24cb9
macaesp <mac.espinoza@duocuc.cl>
2025-05-27 00:13:11 -0400

    commit

--- msvc-producto/src/main/java/com/perfulandia/msvc/producto/MsvcProductoApplication.java
+++ msvc-producto/src/main/java/com/perfulandia/msvc/producto/MsvcProductoApplication.java
@@ -1,9 +1,9 @@
-package com.perulandia.msvc.sucursal;
+package com.perfulandia.msvc.producto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class MsvcProductoApplication {
-
     public static void main(String[] args) { SpringApplication.run(MsvcProductoApplication.class, args); }
 }
