1b3e2223beaacaa985217c019bca09abccbff1ca
macaesp <mac.espinoza@duocuc.cl>
2025-05-27 00:04:13 -0400

    commit

--- msvc-boleta/src/main/java/com/perfulandia/msvc/boleta/model/entities/Boleta.java
+++ msvc-boleta/src/main/java/com/perfulandia/msvc/boleta/model/entities/Boleta.java
@@ -1,11 +1,14 @@
 package com.perfulandia.msvc.boleta.model.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
+@Entity
+@Table(name = "boletas")
+@Getter @Setter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Boleta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
