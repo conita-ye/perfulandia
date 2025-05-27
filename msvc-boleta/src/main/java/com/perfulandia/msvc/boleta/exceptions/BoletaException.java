f1593ce5fec7163db4e176d5a1e02f8a3cb6ac5d
constanza <co.yevenes@duocuc.cl>
2025-05-26 20:59:13 -0400

    commit

--- msvc-boleta/src/main/java/com/perfulandia/msvc/boleta/exceptions/BoletaException.java
+++
@@ -1,7 +0,0 @@
-package com.perfulandia.msvc.boleta.exceptions;
-
-public class BoletaException extends RuntimeException{
-    public BoletaException(String message){
-        super (message);
-    }
-}
