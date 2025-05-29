05b521244251b6a6ef99ddebda88e04a109d3f4c
macaesp <mac.espinoza@duocuc.cl>
2025-05-28 23:46:58 -0400

    commit

--- msvc-inventario/src/main/java/com/perfulandia/msvc/inventario/model/entities/Inventario.java
+++ msvc-inventario/src/main/java/com/perfulandia/msvc/inventario/model/entities/Inventario.java
@@ -1,6 +1,7 @@
 package com.perfulandia.msvc.inventario.model.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -17,6 +18,8 @@ public class Inventario {
 
     @Column (nullable = false)
     @NotBlank(message = "El campo de Stock no puede estar vacio")
+    @NotNull
+    @Min(1)
     private int stock;
 
     @NotNull(message = "El ID del producto no puede ser nulo")
