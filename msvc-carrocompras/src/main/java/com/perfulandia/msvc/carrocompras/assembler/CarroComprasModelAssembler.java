c12344355d6dd1a99f8c81023d5faff94e4594db
constanza <co.yevenes@duocuc.cl>
2025-06-23 23:05:53 -0400

    commit

--- msvc-carrocompras/src/main/java/com/perfulandia/msvc/carrocompras/assembler/CarroComprasModelAssembler.java
+++ msvc-carrocompras/src/main/java/com/perfulandia/msvc/carrocompras/assembler/CarroComprasModelAssembler.java
@@ -1,17 +1,15 @@
 package com.perfulandia.msvc.carrocompras.assembler;
 
-import com.perfulandia.msvc.boleta.controller.CarroComprasController;
+
 import com.perfulandia.msvc.carrocompras.model.entities.CarroCompras;
+
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 
 @Component
-public class CarroComprasModelAssembler implements  {
+public class CarroComprasModelAssembler implements RepresentationModelAssembler<CarroCompras, EntityModel<CarroCompras>> {
 
     @Override
     public EntityModel<CarroCompras> toModel(CarroCompras carroCompras) {
@@ -21,3 +19,4 @@ public class CarroComprasModelAssembler implements  {
                 linkTo(methodOn(com.perfulandgia.msvc.carrocompras.controller.CarroComprasController.class).listarBoleta()).withRel("CarroCompras")
         );
     }
+}
