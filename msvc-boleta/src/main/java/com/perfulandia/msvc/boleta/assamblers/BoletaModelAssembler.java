e6d82049eacbb7e8b74f450f76c23161a7a6d7b1
cely <ara.escobar@duocuc.cl>
2025-06-23 20:58:05 -0400

    commit

--- msvc-boleta/src/main/java/com/perfulandia/msvc/boleta/assamblers/BoletaModelAssembler.java
+++ msvc-boleta/src/main/java/com/perfulandia/msvc/boleta/assamblers/BoletaModelAssembler.java
@@ -1,9 +1,13 @@
 package com.perfulandia.msvc.boleta.assamblers;
 
+import com.perfulandia.msvc.boleta.controller.BoletaController;
 import com.perfulandia.msvc.boleta.model.entities.Boleta;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
 public class BoletaModelAssembler implements RepresentationModelAssembler <Boleta, EntityModel<Boleta>> {
@@ -12,7 +16,9 @@ public class BoletaModelAssembler implements RepresentationModelAssembler <Bolet
     public EntityModel<Boleta> toModel(Boleta boleta) {
         return EntityModel.of(
                 boleta,
-                linkTo );
+                linkTo(methodOn(BoletaController.class).findById(boleta.getIdBoleta())).withSelfRel(),
+                linkTo(methodOn(BoletaController.class).listarBoleta()).withRel("boletas")
+        );
     }
 
 }
---
+++ msvc-cliente/src/main/java/com/perfulandia/msvc/cliente/assemblers/ClienteModelAssembler.java
@@ -0,0 +1,23 @@
+package com.perfulandia.msvc.cliente.assemblers;
+
+import com.perfulandia.msvc.cliente.controller.ClienteController;
+import com.perfulandia.msvc.cliente.model.Cliente;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class ClienteModelAssembler implements RepresentationModelAssembler<Cliente, EntityModel<Cliente>> {
+
+    @Override
+    public EntityModel<Cliente> toModel(Cliente cliente) {
+        return EntityModel.of(
+                cliente,
+                linkTo(methodOn(ClienteController.class).findById(cliente.getIdCliente())).withSelfRel(),
+                linkTo(methodOn(ClienteController.class).listarClientes()).withRel("clientes")
+        );
+    }
+}
