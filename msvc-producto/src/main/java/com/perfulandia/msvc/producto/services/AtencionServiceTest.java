16bc546d665c40a932e3c4460ef724887bddcaec
macaesp <mac.espinoza@duocuc.cl>
2025-06-23 20:24:13 -0400

    commit

---
+++ msvc-producto/src/main/java/com/perfulandia/msvc/producto/services/AtencionServiceTest.java
@@ -0,0 +1,13 @@
+package com.perfulandia.msvc.producto.services;
+
+import com.perfulandia.msvc.producto.service.ProductoService;
+
+@ExtendWith(MockitoExtension.class)
+public class AtencionServiceTest {
+
+    @Mock
+    private ProductoService productoService;
+
+    @Mock
+
+}
