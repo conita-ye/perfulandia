33e5f3801f4820fc0bb9c9ffa8e7c0f68b541cca
cely <ara.escobar@duocuc.cl>
2025-07-10 01:21:41 -0400

    commit

--- msvc-boleta/src/main/java/com/perfulandia/msvc/boleta/assamblers/BoletaModelAssembler.java
+++ msvc-boleta/src/main/java/com/perfulandia/msvc/boleta/assamblers/BoletaModelAssembler.java
@@ -1,6 +1,6 @@
 package com.perfulandia.msvc.boleta.assamblers;
 
-import com.perfulandia.msvc.boleta.controller.BoletaController;
+import com.perfulandia.msvc.boleta.controller.BoletaControllerV2;
 import com.perfulandia.msvc.boleta.model.entities.Boleta;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
@@ -10,15 +10,12 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class BoletaModelAssembler implements RepresentationModelAssembler <Boleta, EntityModel<Boleta>> {
+public class BoletaModelAssembler implements RepresentationModelAssembler<Boleta, EntityModel<Boleta>> {
 
     @Override
     public EntityModel<Boleta> toModel(Boleta boleta) {
-        return EntityModel.of(
-                boleta,
-                linkTo(methodOn(BoletaController.class).findById(boleta.getIdBoleta())).withSelfRel(),
-                linkTo(methodOn(BoletaController.class).listarBoleta()).withRel("boletas")
-        );
+        return EntityModel.of(boleta,
+                linkTo(methodOn(BoletaControllerV2.class).findById(boleta.getIdBoleta())).withSelfRel(),
+                linkTo(methodOn(BoletaControllerV2.class).listarBoleta()).withRel("boletas"));
     }
-
 }
